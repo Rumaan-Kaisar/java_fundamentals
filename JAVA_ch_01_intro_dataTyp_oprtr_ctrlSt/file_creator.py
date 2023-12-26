c3d34563e04de54fb171df61912f6e18959d3c0e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-26 16:30:42 +0600

    Add files via upload

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/file_creator.py
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/file_creator.py
@@ -3,13 +3,23 @@
 
 # Directory: List of the folders you want to create
 file_names = [
-"cpp_ch9_1_OOP_intro",
-"cpp_ch9_2_old_vs_std_header",
-"cpp_ch9_3_func_ovrld",
-"cpp_ch9_4_comments",
-"cpp_ch9_5_cpp_conio",
-"cpp_ch9_6_c_vs_cpp",
-"cpp_ch9_7_cpp_Kywrd"
+    "jv_ch01_01_1_intro_cnvntion",
+    "jv_ch01_01_2_first_run",
+    "jv_ch01_01_3_libs_keywrds",
+    "jv_ch01_01_4_operators",
+    "jv_ch01_01_5_prantsis_space",
+    "jv_ch01_01_6_escape_Seq",
+    "jv_ch01_02_1_if_for_ctrl_logic",
+    "jv_ch01_02_2_if_elif_switch",
+    "jv_ch01_02_3_loop_advanced",
+    "jv_ch01_02_4_break_continue",
+    "jv_ch01_03_1_prmitv_type",
+    "jv_ch01_03_2_litrl_hex_bin_oct",
+    "jv_ch01_03_3_strings",
+    "jv_ch01_03_4_typeCast_convrtn",
+    "jv_ch01_04_1_init_dynmicInit",
+    "jv_ch01_04_2_scope_VarLife",
+    "jv_ch01_05_IO_basic"
 ]
 
 # Parent Directory path
@@ -21,9 +31,10 @@ for i in range (0, len(file_names)):
     # path = "./"+directories[i]
     # Create the files
     try:
-        # os.makedirs(path, exist_ok = True)
-        f = open(f"{file_names[i]}.cpp", "w") # creates an empty 'c++' 
         print(f"{file_names[i]} is created sucuessfully")
+        with open(f"{file_names[i]}.java", "w") as f:     # creates an empty 'java' file
+            f.write("\n/* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= */\n")
+        print(f"{file_names[i]}.py is created sucuessfully")
     except:
         print("files '%s' can not be created" % file_names[i])
 
