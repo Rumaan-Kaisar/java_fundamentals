e0b9dfaaaecc2c3b636517a596c51716ba6a8e68
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-01-11 14:35:12 +0600

    Update jv_ch01_01_3_libs_keywrds.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_01_3_libs_keywrds.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_01_3_libs_keywrds.java
@@ -1,2 +1,22 @@
 
-\* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= *\
+/* -=-=-=-=-=-=-=-=-=-=-=      Keywords & 'Class Libraries'      -=-=-=-=-=-=-=-=-=-=-= 
+
+
+1.5 The Java Keywords
+Fifty keywords are currently defined in the Java language (see Table 1-1). These keywords, combined with the syntax of the operators and separators, form the definition of the Java language. In addition to the keywords, these are values defined by Java: true, false, and null. These keywords and values cannot be used as names for a variable, class, or method.
+
+abstract 	case	continue 	enum 	for 	instanceof 	new 	return	switch 	transient 
+assert 	catch 	default	extends 	goto	int 	package 	short 	synchronized	try 
+boolean 	char 	do 	final	if 	interface 	private 	static 	this 	void
+break 	class 	double 	finally 	implements	long 	protected 	strictfp 	throw 	volatile 
+byte	const 	else 	float 	import 	native	public 	super	throws 	while
+	The keywords const and goto are reserved but not used. 
+	You may not use these words for the names of variables, classes, and so on.
+
+1.6 The Java Class Libraries
+The Java environment relies on several built-in class libraries that contain many built-in methods that provide support for such things as I/O, string handling, networking, and graphics. The standard classes also provide support for a graphical user interface (GUI). Thus, Java as a totality is a combination of the Java language itself, plus its standard classes. As you will see, the class libraries provide much of the functionality that comes with Java. Indeed, part of becoming a Java programmer is learning to use the standard Java classes. 
+	Java is a free-form language, meaning that it does not matter where you place statements relative to each other on a line (multiple statements on a line is acceptable).
+
+
+
+*/
