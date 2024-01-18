329cc62f20c31c5e0d04019747a5e945a21c513b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-01-18 15:30:30 +0600

    Update jv_ch01_01_6_escape_Seq.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_01_6_escape_Seq.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_01_6_escape_Seq.java
@@ -1,2 +1,11 @@
 
-\* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= *\
+/* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= */
+
+1.9 Character escape sequences or backslash character constants 
+Most are same as C/C++. Except octal and hexadecimal (Recall C/C++ 2.7). All presented in the following table.
+Escape Sequence	\'	\"	\\	\r	\n	\f	\t	\b	\ddd	\uxxxx
+Description
+	Single quote	Double quote	Backslash	Carriage return	New line	Form feed	Horizontal tab	Backspace	Octal constant (ddd is an octal)	Hexadecimal constant (xxxx is a hexadecimal)
+
+    
+
