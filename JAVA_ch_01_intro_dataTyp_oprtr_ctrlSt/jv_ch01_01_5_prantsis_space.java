e12c514c66ac9af54d73d0202d7a87a7eeb92f58
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-01-18 15:30:13 +0600

    Update jv_ch01_01_5_prantsis_space.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_01_5_prantsis_space.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_01_5_prantsis_space.java
@@ -1,2 +1,9 @@
 
-\* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= *\
+/* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= */
+
+1.17 Spacing and parenthesis
+The two expressions are the same, but the second is easier to read: 	x=10/y*(127/x);	 x = 10 / y * (127/x);
+	Parentheses increase the precedence of the operations contained within them, just like in algebra. Use of redundant or additional parentheses will not cause errors or slow down the execution of the expression. 
+	Additional spaces makes a good readable code.
+
+
