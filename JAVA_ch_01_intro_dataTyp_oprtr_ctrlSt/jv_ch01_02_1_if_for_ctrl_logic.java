c1acc1246d9c13d4575a142f17cbc9117a0cd52a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-02-12 13:32:20 +0600

    Update jv_ch01_02_1_if_for_ctrl_logic.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_02_1_if_for_ctrl_logic.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_02_1_if_for_ctrl_logic.java
@@ -1,2 +1,18 @@
 
-\* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= *\
+/*  -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= 
+    conditional IF:
+                    if(condition){ 
+                        statement block 
+                    } 
+
+    FOR loop:
+                    for(initialization; condition; iteration){
+                        statement block
+                    }
+
+    Here "condition" is a boolean statement, i.e. 'true' or 'false'.
+    No ";" after statement block's "}". 
+    "statement block" and others are exactly same as C/C++.
+*/
+
+
