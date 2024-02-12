9b577c0a2a2f94872a41855dcab2e38fed83b5ff
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-02-12 13:31:19 +0600

    Update jv_ch01_01_5_prantsis_space.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_01_5_prantsis_space.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_01_5_prantsis_space.java
@@ -1,9 +1,23 @@
 
-/* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= */
+/*  -=-=-=-=-=-=-=-=-=-=-=      Spacing and parenthesis      -=-=-=-=-=-=-=-=-=-=-= 
+    readability:
+        An expression in Java may have tabs and spaces in it to make it more readable
+        The two expressions are the same, but the second is easier to read: 	
+            x=10/y*(127/x);	 
+            x = 10 / y * (127/x);
 
-1.17 Spacing and parenthesis
-The two expressions are the same, but the second is easier to read: 	x=10/y*(127/x);	 x = 10 / y * (127/x);
-	Parentheses increase the precedence of the operations contained within them, just like in algebra. Use of redundant or additional parentheses will not cause errors or slow down the execution of the expression. 
-	Additional spaces makes a good readable code.
+        Additional spaces makes a good readable code.
+
+    precedence:
+        Parentheses 'increase the precedence of the operations' contained within them, just like in ALGEBRA. 
+        Use of 'redundant or additional parentheses' will not cause ERRORS or SLOW down the EXECUTION of the expression. 
+    
+        For example, which of the following two expressions is easier to read?
+
+            x = y/3-34*temp+127;
+            x = (y/3) - (34*temp) + 127;
+*/
+
+
 
 
