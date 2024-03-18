4e5fca8c0d4d1a6789db92cb06ce9f6e36ef9676
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-03-18 19:02:48 +0600

    Update jv_ch01_02_1_if_for_ctrl.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_02_1_if_for_ctrl.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_02_1_if_for_ctrl.java
@@ -228,3 +228,35 @@ class GalToLitTable {
                 A block of code is started with a { and ended with a }. 
 */
 
+
+
+
+/* Example 9: Adapt "Example 6" so that it prints a conversion table of 'inches to meters'. 
+                Display 12 feet of conversions, inch by inch. 
+
+                Output a blank line every 12 inches. 
+                (HINT: One meter equals approximately 39.37 inches.) 
+*/
+
+// This program displays a conversion table of inches to meters.
+
+class InchToMeterTable {
+    public static void main(String args[]) {
+        double inches, meters;
+        int counter;
+
+        counter = 0;    // line counter
+        for(inches = 1; inches <= 144; inches++) {
+            meters = inches/39.37;  // convert to meters
+            System.out.println(inches + " inches is " + meters + " meters.");
+            counter++;
+            // every 12th line, print a blank line
+            if(counter == 12) {
+                System.out.println();
+                counter = 0; // reset the line counter
+            }
+        }
+    }
+}
+
+
