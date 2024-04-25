046334f47dd8f43fea7fed48818eda87259b5870
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-04-25 12:58:06 +0600

    Update jv_ch01_03_2_litrl_hex_bin_oct.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_03_2_litrl_hex_bin_oct.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_03_2_litrl_hex_bin_oct.java
@@ -86,95 +86,9 @@ public class LiteralDemo{
 
 
 
-
 // ----    rev[25-Apr-24]    ----
-
-public class JV_ch1_2_1_primitive_types{
-    public static void main(String[] args) {
-
-        long var3;
-        var3 = 45L;
-        double ver4 = 67;
-        float ver5 = 56F;
-
-        System.out.print("Long int value "+var3+"\n");
-        // You can use the getClass() method to get the type of the object you are using, 
-        // For primitive type we use "Boxing": "(Object)var3" converts 'primitive types' into 'object types'
-        System.out.print("Type of ver3"+((Object)var3).getClass().getName()+"\n");
-        System.out.print("Type of ver4"+((Object)var4).getClass().getName()+"\n");
-        System.out.print("Type of ver5"+((Object)var5).getClass().getName()+"\n");
-
-
-        // String and char: 
-        String s1="hello"; 
-        char ch1='a';
-        System.out.println("Type of s1: "+((Object)s1).getClass().getName());
-        // Following works too: because String is already an Object type, so need to Object-boxing 
-        // System.out.print("Type of s1: "+s1.getClass().getName()+"\n");
-        System.out.println("Type of ch1: "+((Object)ch1).getClass().getName());
-
-
-        int var1; // this declares a variable
-        int var2; // this declares another variable
-        var1 = 1024; // this assigns 1024 to var1
-        System.out.print("var1 contains " + var1+"\n");
-        var2 = var1 / 2;
-        System.out.print("var2 contains var1 / 2: ");
-        System.out.println(var2);
-        // The print() method is just like println(), except that it does not output a new line after each call. 
-        // Both print() and println() can be used to output values of any of Java’s built-in types.
-
-
-        // division on integers
-        int var; // this declares an int variable
-        double x; // this declares a floating-point variable
-        var = 10; // assign var the value 10
-        x = 10.0; // assign x the value 10.0
-        System.out.println("Original value of var: " + var);
-        System.out.println("Original value of x: " + x);
-
-        System.out.println(); // print a blank line
-
-        // now, divide both by 4
-        var = var / 4;
-        x = x / 4;
-        System.out.println("var after division (Notice fraction is lost): " + var);
-        System.out.println("x after division: " + x);
-
-
-        // Gallon to liters
-        double gallons; // holds the number of gallons
-        gallons = 10; // start with 10 gallons
-        double liters = gallons * 3.7854; // Dynamic variable declaration, convert to liters
-        System.out.println(gallons + " gallons is " + liters + " liters.");
-
-
-        // Octal, Hexa decimal, Binary
-        /* Convert int to String: 
-            Using toString() method of "Integer" class
-            Using valueOf() method of "String" class 
-        */
-        int octl = 016;
-        // Octal is coverted to Decimal "by using radix:10" during data-type-conversion to String
-        String octStr = Integer.toString(octl, 10); 
-        System.out.println("Converted num: "+octStr);   // gives 14 in decimal: equivalent to 16 in octal 
-        System.out.println("Type of octStr: "+octStr.getClass().getName()+"\n");
-        // The Integer.parseInt() method converts a string to an int with the given radix
-        int hexd = Integer.parseInt(octStr, 16);    // gives 20 in decimal: equivalent to 14 in Hexadecimal
-        // preceeding line takes the String value as a Hexadecimal number and converts it to Decimal
-        // parseInt(octStr, 16)  consideres the number-string octStr in Hexadecimal
-        System.out.println("Converted dec num to hexd: "+hexd);
-
-        int hex2 = 0x20;
-        String hexStr = Integer.toString(hex2, 10); 
-        System.out.println("Converted num: "+hexStr);   // gives 32 in decimal: equivalent to 20 in Hexadecimal
-        // String hexStr = Integer.toString(hex2, 16); // remains same; Hexadecimal to Hexadecimal 
-    }
-}
-
 
 /* Java Octal to Decimal conversion: Integer.parseInt()
 The Integer.parseInt() method converts a string to an int with the given radix. If you pass 8 as a 'radix', it converts an 'octal' string into 'decimal'. 
 */
-
 
