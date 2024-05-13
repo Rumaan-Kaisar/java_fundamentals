b24810be84c40eeba53ad944d83a3d255bca2327
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-13 14:46:18 +0600

    Update jv_ch01_04_1_dynamic_init.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_04_1_dynamic_init.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_04_1_dynamic_init.java
@@ -1,2 +1,30 @@
 
-\* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= *\
+/* -=-=-=-=-=-=-=-=-=-=-=      variable initialization      -=-=-=-=-=-=-=-=-=-=-= */
+
+
+/* 
+    Initialization: "type var_name = value ;"  Eg:  int count = 10;  char ch = 'X';  float f = 1.2F; 
+    Dynamic Initialization: It is also same for C/C++ local variables. (However global variables do not support dynamic initialization).
+        Java allows variables to be initialized dynamically, using any expression valid at the time the variable is declared (i.e. If the expression variables do not go out of the scope. Because local variables destroyed after the function call). For example, 
+
+        double radius = 4, height = 5;  //  these variables must be declared before "volume"
+        double volume = 3.1416 * radius * radius * height; // dynamically initialize volume
+
+
+
+        
+
+        	Initialization is same as C/C++: 	type var_name = value ;  Eg:  int count = 10;  char ch = 'X';  float f = 1.2F; 
+	Using a comma-separated list, you can give one or more same type variables an initial value. For example:     int a, b = 8, c = 19, d;
+In this case, only b and c are initialized.
+	Dynamic Initialization: It is also same for C/C++ local variables. However global variables do not support dynamic initialization (initialize a global variable using other variable is not supported, Recall 3.4 Variable initialization C/C++). 
+	Java allows variables to be initialized dynamically, using any expression valid at the time the variable is declared (i.e. If the expression variables do not go out of the scope. Because local variables destroyed after the function call). For example, 
+
+double radius = 4, height = 5;
+double volume = 3.1416 * radius * radius * height; // dynamically initialize volume
+
+
+        */
+
+
+
