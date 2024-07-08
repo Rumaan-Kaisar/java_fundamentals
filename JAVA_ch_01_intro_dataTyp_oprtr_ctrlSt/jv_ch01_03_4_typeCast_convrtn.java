b96bbcde2678d9c7478ae2b2625de70055f715f6
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-07-08 15:20:39 +0600

    Update jv_ch01_03_4_typeCast_convrtn.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_03_4_typeCast_convrtn.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_03_4_typeCast_convrtn.java
@@ -111,21 +111,9 @@
                 For a   "double"  operand, whole expression is promoted to "double"
 
 
-// ----  rev[04-jul-2024]  ----
+        Type promotions of a variable "EXPIRE" after the evaluation of the expression. 
+        For example, if the value of a "byte variable" is promoted to int inside an expression, 
+            outside the expression, the variable is still a byte. 
 
-	type promotions of a variable expire after the evaluation of the expression. For example, if the value of a byte variable is promoted to int inside an expression, outside the expression, the variable is still a byte. Type promotion only affects the evaluation of an expression.
-	type-cast in expression: Even though we have type-promotion in an expression, but in some cases we still need type-cast. Eg:
-byte b;   int i;
-b = 10;   i = b * b; 		// OK, no cast needed
-b = 10;   b = (byte) (b * b); 	// cast needed!!
-	No cast is needed when assigning b*b to i, because b is promoted to int when the expression is evaluated. 
-	However, when you try to assign b*b to b, you do need a cast—back to byte! Keep this in mind if you get unexpected type-incompatibility error messages on expressions that would otherwise seem perfectly OK.
-	This same sort of situation also occurs when performing operations on chars. For example, in the following fragment, the cast back to char is needed because of the promotion of ch1 and ch2 to int within the expression:
-char ch1 = 'a',   ch2 = 'b';
-ch1 = (char) (ch1 + ch2);
-Without the cast, the result of adding ch1 to ch2 would be int, which can’t be assigned to a char.
-
-
-
+        Type promotion only affects the evaluation of an expression.
 */
-
