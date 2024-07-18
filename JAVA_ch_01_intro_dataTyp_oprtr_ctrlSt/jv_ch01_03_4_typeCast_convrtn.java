f9bed9e42bcdcd30970e522700e5df828b99022e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-07-18 09:44:31 +0600

    Update jv_ch01_03_4_typeCast_convrtn.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_03_4_typeCast_convrtn.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_03_4_typeCast_convrtn.java
@@ -232,3 +232,35 @@ class DtoL {
     }
 }
 
+
+
+
+/* Example 4: The following program demonstrates some type conversions that require casts */
+
+class CastDemo {
+    public static void main(String args[]) {
+        double x, y;
+        byte b;
+        int i;
+        char ch;
+
+        x = 10.0;
+        y = 3.0;
+        i = (int)(x/y); // cast double to int. Truncation will occur
+        System.out.println("Integer outcome of x / y: " + i);
+
+        i = 100;
+        b = (byte)i;  // No loss of info here. A byte can hold the value 100
+        System.out.println("Value of b: " + b);
+
+        i = 257;
+        b = (byte)i;  // Information loss this time. A byte cannot hold the value 257
+        System.out.println("Value of b: " + b);
+
+        b = 88; // ASCII code for X
+        ch = (char)b;   // Cast between incompatible types
+        System.out.println("ch: " + ch);
+    }
+}
+
+
