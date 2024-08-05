c886c71a637921d0145555e68d0295ffd7b1f88e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-05 08:18:06 +0600

    Update jv_ch01_03_4_typeCast_convrtn.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_03_4_typeCast_convrtn.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_03_4_typeCast_convrtn.java
@@ -283,15 +283,16 @@ class CastDemo {
 */
 // A promotion surprise!
 class PromDemo {
-public static void main(String args[]) {
-byte b;
-int i;
-b = 10;
-i = b * b; // OK, no cast needed
-b = 10;
-b = (byte) (b * b); // cast needed!!
-System.out.println("i and b: " + i + " " + b);
-}
+    public static void main(String args[]) {
+        byte b;
+        int i;
+
+        b = 10;
+        i = b*b;    // OK, no cast needed
+        b = 10;
+        b = (byte)(b * b);     // cast needed!!
+        System.out.println("i and b: " + i + " " + b);
+    }
 }
 
 
