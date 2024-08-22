7a4d8ff934eea05009350bb2980e5d9b06a0737a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-22 15:49:39 +0600

    Update jv_ch01_selfTest.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_selfTest.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_selfTest.java
@@ -1,36 +1,6 @@
 
 /* ------------    Chapter 2 Self Test    ------------
 
-// ----  rev[19-Aug-2024]  ----
-
-
-Example 9: In general, when is a cast needed?
-A cast is needed when converting between incompatible types or when a narrowing conversion is
-occurring.
-
-
-
-
-Example 10: Write a program that finds all of the prime numbers between 2 and 100.
-// Find prime numbers between 2 and 100.
-class Prime {
-public static void main(String args[]) {
-int i, j;
-boolean isprime;
-for(i=2; i < 100; i++) {
-isprime = true;
-// see if the number is evenly divisible
-for(j=2; j <= i/j; j++)
-// if it is, then it's not prime
-if((i%j) == 0) isprime = false;
-if(isprime)
-System.out.println(i + " is prime.");
-}
-}
-}
-
-
 */
-
 
 
