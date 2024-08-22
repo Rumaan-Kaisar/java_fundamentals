477441ea2a7da0274f168125727bb818240b7638
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-22 15:39:46 +0600

    Update jv_ch01_02_1_if_for_ctrl.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_02_1_if_for_ctrl.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_02_1_if_for_ctrl.java
@@ -260,3 +260,24 @@ class InchToMeterTable {
 }
 
 
+
+
+/*  Example 10: Write a program that finds all of the prime numbers between 2 and 100. */
+
+// Find prime numbers between 2 and 100.
+class Prime {
+    public static void main(String args[]) {
+        int i, j;
+        boolean isprime;
+
+        for(i=2; i < 100; i++) {
+            isprime = true;
+            // see if the number is evenly divisible. If it is, then it's not prime
+            for(j=2; j <= i/j; j++) if((i%j) == 0) isprime = false;
+        
+            if(isprime) System.out.println(i + " is prime.");
+        }
+    }
+}
+
+
