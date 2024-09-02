dca1d4b7194abf04b3bff2cb77c7f98a2025f66b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-02 15:49:06 +0600

    Update jv_ch01_05_IO_basic.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_05_IO_basic.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_05_IO_basic.java
@@ -1,17 +1,33 @@
 
 /* -=-=-=-=-=-=-=-=-=-=-=      Keyboard input      -=-=-=-=-=-=-=-=-=-=-= 
+    
+    Input Characters from the Keyboard:
+        To read a character from the keyboard, we will use
+            
+                System.in.read()
+
+        By default, console input is line buffered.
 
 
-Input Characters from the Keyboard:
-To read a character from the keyboard, we will use System.in.read().  
-By default, console input is line buffered.
+        "System.in" is the "input object" attached to the keyboard
+        The read() method waits until the user "presses a key" and then "returns" the result
+        The "character" is returned as an "int", so it must be "cast into a char" to assign it to a "char" variable
+
+        Buffer:
+            In simple terms, a buffer is like a temporary storage area in your computer's memory. 
+            
+            When you type something on the keyboard, the characters you type are 
+                first collected in this storage area, or "buffer," before they are sent to your program.
+            
+            You can think of a buffer as a "waiting room" for the data you enter, 
+                where it stays temporarily until it's ready to be used by your program.
+
+            For example, when you're typing on the console, the characters you enter don't go directly to your program one by one. 
+                Instead, they are stored in the buffer until you press the ENTER key. 
 
 
-System.in is the input object attached to the keyboard. 
-The read() method waits until the user presses a key and then returns the result.  
-The character is returned as an integer, so it must be cast into a char to assign it to a char variable.  (Not useful for int)
 
-[Here, the term buffer refers to a small portion of memory that is used to hold the characters before they are read by your program. In this case, the buffer holds a complete line of text. As a result, you must press ENTER before any character that you type will be sent to your program. ]
+
 
 
 	Notice that  main()  begins like this: 	public static void main(String args[])
