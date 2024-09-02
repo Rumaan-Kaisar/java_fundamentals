12ed4224a331d9e76d096a4e789ed812ee9d03b0
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-02 16:01:11 +0600

    Update jv_ch01_05_IO_basic.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_05_IO_basic.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_05_IO_basic.java
@@ -48,7 +48,8 @@ class KbIn {
         char ch;
         
         System.out.print("Press a key followed by ENTER: ");
-        ch = (char) System.in.read(); 	// get a char. Read a character from the keyboard.
+        // notice the cast
+        ch = (char) System.in.read(); 	// get a char, Read a character from the keyboard
         System.out.println("Your key is: " + ch); 	
     } 
 }
