bf6a04b447106db7e43193b6016e328bbc51e7e4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-02 15:34:25 +0600

    Update jv_ch01_05_IO_basic.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_05_IO_basic.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_05_IO_basic.java
@@ -3,8 +3,14 @@
 
 
 Input Characters from the Keyboard:
-To read a character from the keyboard, we will use System.in.read().  By default, console input is line buffered. 
-	System.in is the input object attached to the keyboard. The read() method waits until the user presses a key and then returns the result.  The character is returned as an integer, so it must be cast into a char to assign it to a char variable.  (Not useful for int)
+To read a character from the keyboard, we will use System.in.read().  
+By default, console input is line buffered.
+
+
+System.in is the input object attached to the keyboard. 
+The read() method waits until the user presses a key and then returns the result.  
+The character is returned as an integer, so it must be cast into a char to assign it to a char variable.  (Not useful for int)
+
 [Here, the term buffer refers to a small portion of memory that is used to hold the characters before they are read by your program. In this case, the buffer holds a complete line of text. As a result, you must press ENTER before any character that you type will be sent to your program. ]
 
 
