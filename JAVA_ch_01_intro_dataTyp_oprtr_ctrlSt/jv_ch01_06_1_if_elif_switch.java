0cf986ab1e4e484b61fd21c9d2ac60bf29dffc6e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-10-10 15:16:30 +0600

    Update jv_ch01_06_1_if_elif_switch.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_06_1_if_elif_switch.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_06_1_if_elif_switch.java
@@ -542,3 +542,55 @@ public class NestedSwitchExample2 {
     }
 }
 
+
+
+
+/* Example 11: Build a simple help system that displays the syntax for the Java control statements. 
+                The program displays a menu containing the control statements and then waits for you to choose one. 
+
+                After one is chosen, the syntax of the statement is displayed. 
+                In this first version of the program, help is available for only the if and switch statements.
+*/
+
+// ----  rev[10-Oct-2024]  ----
+
+// A simple help system.
+
+class Help {
+public static void main(String args[])
+throws java.io.IOException {
+char choice;
+System.out.println("Help on:");
+System.out.println(" 1. if");
+System.out.println(" 2. switch");
+System.out.print("Choose one: ");
+choice = (char) System.in.read();
+System.out.println("\n");
+switch(choice) {
+case '1':
+System.out.println("The if:\n");
+System.out.println("if(condition) statement;");
+System.out.println("else statement;");
+break;
+case '2':
+System.out.println("The switch:\n");
+System.out.println("switch(expression) {");
+System.out.println(" case constant:");
+System.out.println(" statement sequence");
+System.out.println(" break;");
+System.out.println(" // ...");
+System.out.println("}");
+break;
+default:
+System.out.print("Selection not found.");
+}
+}
+}Chapter 3: Program Control Statements 75
+6. Here is a sample run.
+Help on:
+1. if
+2. switch
+Choose one: 1
+The if:
+if(condition) statement;
+else statement
