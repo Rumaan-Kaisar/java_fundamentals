38e81831e1350a912afb91c47bcd278a6536bf04
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-12-16 15:08:56 +0600

    Update jv_ch01_06_3_break_continue.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_06_3_break_continue.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_06_3_break_continue.java
@@ -85,33 +85,91 @@
             }
             System.out.println("Loops complete.");
 
+        The break statement in the inner loop causes the termination of only that loop. 
+        The outer loop is unaffected. 
 
-// ----  rev[12-Dec-2024]  ----
 
-
-The break statement in the inner loop causes the termination of only that loop. 
-The outer loop is unaffected. 
-
-The break that terminates a switch statement affects only that switch statement and not any enclosing loops.
-
+    Break in switch:
+        The break that terminates a "switch" statement affects only that switch statement and not any enclosing loops.
+*/
 
 
 
-1.25  "break" and "continue" with LABEL (Replacing "goto" )
-	Labeled break:  The break can be employed by itself to provide a “civilized” form of the goto. 
+
+/* Example 1: The break statement stops a loop immediately and moves program control to the statement after the loop.
+                Notice following program, the loop is set to run from 0 to 100,
+                    but it ends early when i*i becomes greater than or equal to "num", (which in this case is 100)
+                    the break statement causes it to terminate early
+
+                Output:
+                    0 1 2 3 4 5 6 7 8 9 Loop complete
+*/
+
+class BreakDemo {
+    public static void main(String args[]) {
+        int num;
+
+        num = 100;
+        // loop while i-squared is less than num
+        for(int i=0; i < num; i++) {
+            if(i*i >= num) break; // terminate loop if i*i >= 100
+            System.out.print(i + " ");
+        }
+        System.out.println("Loop complete.");
+    }
+}
+
+
+
+
+/* Example 2: The break statement used to terminate intentionally infinite loops. 
+                For example, the following program simply reads input until the user types the letter q.
+*/
+
+class Break2 {
+    public static void main(String args[]) throws java.io.IOException {
+        char ch;
+
+        for( ; ; ) {
+            ch = (char) System.in.read(); // get a char
+            if(ch == 'q') break;
+        }
+        System.out.println("You pressed q!");
+    }
+}
+
+
+
+
+
+// ----  rev[16-Dec-2024]  ----
+
+/* 
+------------    "break" and "continue" with LABEL (Replacing "goto")    ------------
+
+-=-=-=-    Labeled break    -=-=-=-
+
+	Labeled break:  
+The break can be employed by itself to provide a “civilized” form of the goto. 
 Java does not have goto. There are, however, a few places where the goto is a useful, eg: the goto can be helpful when exiting from a deeply nested set of loops. To handle such situations, Java defines an expanded form of break. 
 
 	By using this form of break, you can, for example, break out of one or more blocks of code. 
 These blocks need not be part of a loop or a switch. They can be any block. It gives you the benefits of a goto without its problems.
 
 	Further, you can specify precisely where execution will resume, because this form of break works with a label. 
-	The general form of the labeled break, 	break label;
-	Typically, label is the name of a label that identifies a block of code. When this form of break executes, control is transferred out of the named block of code. 
+
+
+	The general form of the labeled break:
+            break label;
+
+            	Typically, label is the name of a label that identifies a block of code. When this form of break executes, control is transferred out of the named block of code. 
 
 	The labeled block of code must enclose the break, but it does not need to be the immediately enclosing block. 
 This means that you can use a labeled break to exit from a set of nested blocks. But you cannot use break to transfer control to a block of code that does not enclose the break.
 
 	To name a block, put a label at the start of it. The block can be a stand-alone block, or a statement that has a block as its target. A label is any valid Java identifier followed by a colon. Once you have labeled a block, you can then use this label as the target of a break. Doing so causes execution to resume at the end of the labeled block. 
+
+
 
 	Example 1: Following program shows three nested blocks:
 for(i=1; i<4; i++) { one: { two: { three: { System.out.println("\n i is " + i);
@@ -141,6 +199,9 @@ System.out.println("After for.");	Output:
 	When i is 1, the first if succeeds, causing a break to the end of the block of code defined by label one. This causes After block one. to print. 
 	When i is 2, the second if succeeds, causing control to be transferred to the end of the block labeled by two. This causes the messages After block two. and After block one. to be printed, in that order. 
 	When i is 3, the third if succeeds, and control is transferred to the end of the block labeled by three. Now, all three messages are displayed.
+
+
+
 	Example 2: This time, break is being used to jump outside of a series of nested for loops. When the break statement in the inner loop is executed, program control jumps to the end of the block defined by the outer for loop, which is labeled by done. 
 
 done: for(int i=0; i<10; i++) { 
@@ -160,7 +221,11 @@ System.out.println("After i loop");	Output:
 	4
 	5
 	After i loop
-	Example 3: Precisely where you put a label is very important—especially when working with loops. For example:
+
+
+
+
+    	Example 3: Precisely where you put a label is very important—especially when working with loops. For example:
 
 // here, put label before for statement.
 stop1: for(x=0; x < 5; x++) 
@@ -188,12 +253,20 @@ stop2: {	 for(y = 0; y < 5; y++) { if(y == 2) break stop2;
 		x and y: 4 1
 	Both sets of nested loops are the same except for one point. In the first set, the label precedes the outer for loop. In this case, when the break executes, it transfers control to the end of the entire for block, skipping the rest of the outer loop’s iterations.
 	In the second set, the label precedes the outer for’s opening curly brace. Thus, when break stop2 executes, control is transferred to the end of the outer for’s block, causing the next iteration to occur.
+
+
 	Example 4: We cannot break to any label that is not defined for an enclosing block. Eg:  following is invalid & won't compile:
 
 one: 	for(int i=0; i<3; i++) { System.out.print("Pass " + i + ": "); }
 for(int j=0; j<100; j++) {	if(j == 10) break one; // WRONG
 System.out.print(j + " "); }
 	Since the loop labeled one does not enclose the break, it is not possible to transfer control to that block.
+
+
+
+-=-=-=-    Labeled continue    -=-=-=-
+
+
 	Labeled continue:   As with the break statement, continue may specify a label to describe which enclosing loop to continue. Eg:
 outerloop: 
 for(int i=1; i < 10; i++) { System.out.print("\nOuter loop pass " + i + ", Inner loop: ");
@@ -291,49 +364,6 @@ By remembering these rules, you'll handle break and continue effectively in Java
 
 */
 
-
-
-
-/* Example 1: The break statement stops a loop immediately and moves program control to the statement after the loop.
-                Notice following program, the loop is set to run from 0 to 100,
-                    but it ends early when i*i becomes greater than or equal to "num", (which in this case is 100)
-                    the break statement causes it to terminate early
-
-                Output:
-                    0 1 2 3 4 5 6 7 8 9 Loop complete
-*/
-
-class BreakDemo {
-    public static void main(String args[]) {
-        int num;
-
-        num = 100;
-        // loop while i-squared is less than num
-        for(int i=0; i < num; i++) {
-            if(i*i >= num) break; // terminate loop if i*i >= 100
-            System.out.print(i + " ");
-        }
-        System.out.println("Loop complete.");
-    }
-}
-
-
-
-
-/* Example 2: The break statement used to terminate intentionally infinite loops. 
-                For example, the following program simply reads input until the user types the letter q.
-*/
-class Break2 {
-    public static void main(String args[]) throws java.io.IOException {
-        char ch;
-
-        for( ; ; ) {
-            ch = (char) System.in.read(); // get a char
-            if(ch == 'q') break;
-        }
-        System.out.println("You pressed q!");
-    }
-}
 
 
 
