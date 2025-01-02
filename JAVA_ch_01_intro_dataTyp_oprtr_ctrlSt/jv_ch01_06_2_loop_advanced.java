c09ff54145aebc2aacab0f0919488d4efef662b6
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-02 12:44:27 +0600

    Update jv_ch01_06_2_loop_advanced.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_06_2_loop_advanced.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_06_2_loop_advanced.java
@@ -696,3 +696,25 @@ class Help2 {
 
 
 
+/* Example 13: following program uses a nested for loop to find the factors of
+the numbers from 2 to 100: */
+/*
+Use nested loops to find factors of numbers
+between 2 and 100.
+*/
+class FindFac {
+public static void main(String args[]) {
+for(int i=2; i <= 100; i++) {
+System.out.print("Factors of " + i + ": ");
+for(int j = 2; j < i; j++)
+if((i%j) == 0) System.out.print(j + " ");
+System.out.println();
+}
+}
+}
+
+
+
+/* Example 13: Extra challenge: The preceding
+program can be made more efficient. Can you see how? (Hint: The number of iterations in the
+inner loop can be reduced.) */
