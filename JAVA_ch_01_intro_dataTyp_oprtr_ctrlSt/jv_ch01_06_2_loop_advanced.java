71f794a1ffad6423fc50aa32747709e1ea3e4d72
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-09 12:51:33 +0600

    Update jv_ch01_06_2_loop_advanced.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_06_2_loop_advanced.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_06_2_loop_advanced.java
@@ -778,7 +778,7 @@ class Spaces {
 
 
 
-/* Example 16: Show the for statement for a loop that counts from 1000 to 0 by –2.
+/* Example 16: Show the "FOR" statement for a loop that counts from 1000 to 0 by –2.
 
                 for(int i = 1000; i >= 0; i -= 2)   // ...
 */
