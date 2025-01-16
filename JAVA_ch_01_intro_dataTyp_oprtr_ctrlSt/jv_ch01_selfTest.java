a29238e6227412f40307a36ec4b12ee31f4df2a2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-16 10:39:36 +0600

    Update jv_ch01_selfTest.java

--- JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_selfTest.java
+++ JAVA_ch_01_intro_dataTyp_oprtr_ctrlSt/jv_ch01_selfTest.java
@@ -1,42 +1,6 @@
 
 /* ------------    Chapter 3 Self Test    ------------
 
-6. Explain what break does. Be sure to explain both of its forms.
-
-
-
-
-7. In the following fragment, after the break statement executes, what is displayed?
-for(i = 0; i < 10; i++) {
-while(running) {
-if(x<y) break;
-// ...
-}
-System.out.println("after while");
-}
-System.out.println("After for");
-
-
-
-
-8. What does the following fragment print?
-for(int i = 0; i<10; i++) {
-System.out.print(i + " ");
-if((i%2) == 0) continue;
-System.out.println();
-}
-✓Chapter 3: Program Control Statements 101
-
-
-
-
-9. The iteration expression in a for loop need not always alter the loop control variable by a
-fixed amount. Instead, the loop control variable can change in any arbitrary way. Using this
-concept, write a program that uses a for loop to generate and display the progression 1, 2, 4,
-8, 16, 32, and so on.
-
-
-
 
 10. The ASCII lowercase letters are separated from the uppercase letters by 32. Thus, to convert
 a lowercase letter to uppercase, subtract 32 from it. Use this information to write a program
@@ -45,74 +9,11 @@ and all uppercase letters to lowercase, displaying the result. Make no changes t
 character. Have the program stop when the user enters a period. At the end, have the program
 display the number of case changes that have taken place.
 
-
 */
 
 
 
-
-
-
-/*
-
-6. Explain what break does. Be sure to explain both of its forms.
-A break without a label causes termination of its immediately enclosing loop or switch statement.
-A break with a label causes control to transfer to the end of the labeled block.Appendix A: Answers to Self Tests 621
-
-
-
-
-7. In the following fragment, after the break statement executes, what is displayed?
-for(i = 0; i < 10; i++) {
-while(running) {
-if(x<y) break;
-// ...
-}
-System.out.println("after while");
-}
-System.out.println("After for");
-After break executes, “after while” is displayed.
-
-
-
-
-8. What does the following fragment print?
-for(int i = 0; i<10; i++) {
-System.out.print(i + " ");
-if((i%2) == 0) continue;
-System.out.println();
-}
-Here is the answer:
-0 1
-2 3
-4 5
-6 7
-8 9
-
-
-
-
-
-9. The iteration expression in a for loop need not always alter the loop control variable by a
-fixed amount. Instead, the loop control variable can change in any arbitrary way. Using this
-concept, write a program that uses a for loop to generate and display the progression 1, 2, 4,
-8, 16, 32, and so on.
-
-
-// Use a for loop to generate the progression
-1 2 4 8 16, ...
-
-class Progress {
-public static void main(String args[]) {
-for(int i = 1; i < 100; i += i)
-System.out.print(i + " ");
-}
-}
-
-
-
-
-10. The ASCII lowercase letters are separated from the uppercase letters by 32. Thus, to convert
+/* Example: 10. The ASCII lowercase letters are separated from the uppercase letters by 32. Thus, to convert
 a lowercase letter to uppercase, subtract 32 from it. Use this information to write a program
 that reads characters from the keyboard. Have it convert all lowercase letters to uppercase,622 Java: A Beginner’s Guide
 and all uppercase letters to lowercase, displaying the result. Make no changes to any other
@@ -142,6 +43,6 @@ System.out.println("Case changes: " + changes);
 }
 }
 
-
-
 */
+
+
