99172f819a1c2a27f9962c3bbad377b1edfe930b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-03-27 14:53:59 +0600

    Update jv_ch02_03_1_methods_return.java

--- JAVA_ch_02_class_obj_array/jv_ch02_03_1_methods_return.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_03_1_methods_return.java
@@ -1,8 +1,7 @@
+
 // 110.3-115.9
+
 /* -=-=-=-=-=-=-=-=-=-=-=      Methods and returning from methods      -=-=-=-=-=-=-=-=-=-=-= 
-
-    Methods in Java Classes:
-
 
     ------    Methods in Java vs C++ functions    ------
     "Methods in Java" are similar to "functions in C/C++", but there are key differences:
@@ -50,58 +49,35 @@
 
 
 
-// ----  rev[24-Mar-2025]  ----
+            
+    ----------------    Methods in Java    ----------------
+
+    The general form of a Java method is:
+
+            ret_type name(parameter_list){
+                // body of method 
+            }
+
+        The "ret_type" is the return type of the function. 
+        If the function returns no value then the ret_type must be void.
+
+
+    Note:
+        In C/C++ we can define a function "inside" of a class or "outside" of a class. 
+        We used the scope resolution operator (::) to define a function outside of class. 
+        But in Java it is un-common. In Java methods are usually defined inside a class. 
+
+        So it is un-common to use the scope resolution operator (::) to define a method outside of its class.  
 
 
 
+    ----  Returning  ----
 
+    Returning from a Method: 
+        there are two conditions that "cause" a method to return
 
-
-
-
-
-
-
-As explained, instance variables and methods are constituents of classes. So far, the Vehicle
-class contains data, but no methods. Although data-only classes are perfectly valid, most
-classes will have methods. Methods are subroutines that manipulate the data defined by
-the class and, in many cases, provide access to that data. In most cases, other parts of your
-program will interact with a class through its methods.
-A method contains one or more statements. In well-written Java code, each method
-performs only one task. Each method has a name, and it is this name that is used to call the
-method. In general, you can give a method whatever name you please. However, remember
-that main( ) is reserved for the method that begins execution of your program. Also, don’t use
-Java’s keywords for method names.
-When denoting methods in text, this book has used and will continue to use a convention that
-has become common when writing about Java. A method will have parentheses after its name.
-For example, if a method’s name is getval, it will be written getval( ) when its name is used in a
-sentence. This notation will help you distinguish variable names from method names in this book.
-The general form of a method is shown here:
-ret-type name( parameter-list ) {
-// body of method
-}
-Here, ret-type specifies the type of data returned by the method. This can be any valid type,
-including class types that you create. If the method does not return a value, its return type
-must be void. The name of the method is specified by name. This can be any legal identifier
-other than those already used by other items within the current scope. The parameter-list is a
-sequence of type and identifier pairs separated by commas. Parameters are essentially variables
-that receive the value of the arguments passed to the method when it is called. If the method
-has no parameters, the parameter list will be empty.
-
-
-Methods are similar to C/C++'s "functions". The general form of a Java method is:
-
-ret_type name(parameter_list){
-    // body of method 
-}
-
-The ret-type is the return type of the function. If the function returns no value then the ret-type must be void.
-	Note: In C/C++ we can define a function inside of a class or outside of a class. We used the scope resolution operator (::) to define a function outside of class. But in Java it is un-common. In Java methods are usually defined inside a class, so it is un-common to use the scope resolution operator (::) to define a method outside of its class.  
-
-
-	Returning from a Method: there are two conditions that cause a method to return
-[1]	First, when the method’s closing curly brace is encountered. 
-[2]	Second, when a return statement is executed. 
+        [1]     First, when the method’s closing curly brace is encountered. 
+        [2]     Second, when a return statement is executed. 
 
 
 	Return : There are two forms of return:
@@ -115,6 +91,42 @@ int range() { return x * y; }.
 double devide(int x, int y) { return (double) (x /y); }
 Here a type cast is used to return "double value" from int values (which are int parameters of the method)
 	It is permissible to have multiple return statements in a method, however, because having too many exit points (i.e. return statements) in a method can destructure code. A well-designed method has well-defined exit points.
+
+
+
+
+
+Summary of Java Methods
+Methods and Instance Variables: Methods and instance variables are part of a class. While a class can contain only data, most classes include methods to manipulate and access that data.
+
+Role of Methods: Methods serve as subroutines that interact with a class's data. Other parts of a program typically access class data through its methods.
+
+Naming and Structure:
+
+Each method has a name used to call it.
+
+The main() method is reserved for program execution.
+
+Java keywords cannot be used as method names.
+
+Method Notation: Methods are written with parentheses, e.g., getval(), to differentiate them from variables.
+
+General Method Form:
+
+java
+Copy
+Edit
+ret-type name(parameter-list) {
+    // body of method
+}
+ret-type: The return type of the method (can be any valid type, including class types).
+
+name: A unique identifier for the method.
+
+parameter-list: A list of input values (can be empty if no parameters are needed).
+
+Method Parameters: Parameters receive values passed when calling the method. If no parameters are required, the list remains empty.
+
 
 
 
