8e5ffd050c7856a16784cf96e48c9cd12eb27655
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-03 15:13:56 +0600

    Update jv_ch02_03_1_methods_return.java

--- JAVA_ch_02_class_obj_array/jv_ch02_03_1_methods_return.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_03_1_methods_return.java
@@ -167,3 +167,209 @@
 */
 
 
+// consider the "Vehicle Class" examples in jv_ch02_01_class_in_Java.java
+
+
+Adding a Method to the Vehicle Class
+As just explained, the methods of a class typically manipulate and provide access to the data of
+the class. 
+
+
+With this in mind, recall that main( ) in the preceding examples computed the range of
+a vehicle by multiplying its fuel consumption rate by its fuel capacity. While technically correct,Chapter 4: Introducing Classes, Objects, and Methods 111
+this is not the best way to handle this computation. The calculation of a vehicle’s range is
+something that is best handled by the Vehicle class itself. The reason for this conclusion is easy
+to understand: the range of a vehicle is dependent upon the capacity of the fuel tank and the
+rate of fuel consumption, and both of these quantities are encapsulated by Vehicle. By adding
+a method to Vehicle that computes the range, you are enhancing its object-oriented structure.
+To add a method to Vehicle, specify it within Vehicle’s declaration. For example, the following
+version of Vehicle contains a method called range( ) that displays the range of the vehicle.
+
+
+// Add range to Vehicle.
+class Vehicle {
+int passengers; // number of passengers
+int fuelcap; // fuel capacity in gallons
+int mpg; // fuel consumption in miles per gallon
+// Display the range.
+void range() {  // The range( ) method is contained within the Vehicle class.
+System.out.println("Range is " + fuelcap * mpg);    // Notice that fuelcap and mpg are used directly, without the dot operator.
+}
+}
+class AddMeth {
+public static void main(String args[]) {
+Vehicle minivan = new Vehicle();
+Vehicle sportscar = new Vehicle();
+int range1, range2;
+// assign values to fields in minivan
+minivan.passengers = 7;
+minivan.fuelcap = 16;
+minivan.mpg = 21;
+// assign values to fields in sportscar
+sportscar.passengers = 2;
+sportscar.fuelcap = 14;
+sportscar.mpg = 12;
+System.out.print("Minivan can carry " + minivan.passengers +
+". ");
+minivan.range(); // display range of minivan
+System.out.print("Sportscar can carry " + sportscar.passengers +
+". ");
+sportscar.range(); // display range of sportscar.
+}
+}
+
+
+
+
+
+
+This program generates the following output:
+Minivan can carry 7. Range is 336
+Sportscar can carry 2. Range is 168
+
+
+
+Let’s look at the key elements of this program, beginning with the range( ) method itself.
+The first line of range( ) is
+void range() {
+This line declares a method called range that has no parameters. Its return type is void. Thus,
+range( ) does not return a value to the caller. The line ends with the opening curly brace of the
+method body.
+
+
+The body of range( ) consists solely of this line:
+System.out.println("Range is " + fuelcap * mpg);
+This statement displays the range of the vehicle by multiplying fuelcap by mpg. Since each
+object of type Vehicle has its own copy of fuelcap and mpg, when range( ) is called, the range
+computation uses the calling object’s copies of those variables.
+The range( ) method ends when its closing curly brace is encountered. This causes
+program control to transfer back to the caller.
+Next, look closely at this line of code from inside main( ):
+minivan.range();
+This statement invokes the range( ) method on minivan. That is, it calls range( ) relative to
+the minivan object, using the object’s name followed by the dot operator. When a method is
+called, program control is transferred to the method. When the method terminates, control is
+transferred back to the caller, and execution resumes with the line of code following the call.
+In this case, the call to minivan.range( ) displays the range of the vehicle defined by minivan.
+In similar fashion, the call to sportscar.range( ) displays the range of the vehicle defined by
+sportscar. Each time range( ) is invoked, it displays the range for the specified object.
+There is something very important to notice inside the range( ) method: the instance
+variables fuelcap and mpg are referred to directly, without preceding them with an object name
+or the dot operator. When a method uses an instance variable that is defined by its class, it does
+so directly, without explicit reference to an object and without use of the dot operator. This is
+easy to understand if you think about it. A method is always invoked relative to some object of
+its class. Once this invocation has occurred, the object is known. Thus, within a method, there is
+no need to specify the object a second time. This means that fuelcap and mpg inside range( )
+implicitly refer to the copies of those variables found in the object that invokes range( ).
+Returning from a Method
+In general, there are two conditions that cause a method to return—first, as the range( ) method
+in the preceding example shows, when the method’s closing curly brace is encountered. The
+second is when a return statement is executed. There are two forms of return—one for use inChapter 4: Introducing Classes, Objects, and Methods 113
+void methods (those that do not return a value) and one for returning values. The first form is
+examined here. The next section explains how to return values.
+In a void method, you can cause the immediate termination of a method by using this form
+of return:
+return ;
+When this statement executes, program control returns to the caller, skipping any remaining
+code in the method. For example, consider this method:
+void myMeth() {
+int i;
+for(i=0; i<10; i++) {
+if(i == 5) return; // stop at 5
+System.out.println();
+}
+}
+Here, the for loop will only run from 0 to 5, because once i equals 5, the method returns. It is
+permissible to have multiple return statements in a method, especially when there are two or
+more routes out of it. For example:
+void myMeth() {
+// ...
+if(done) return;
+// ...
+if(error) return;
+// ...
+}
+Here, the method returns if it is done or if an error occurs. Be careful, however, because
+having too many exit points in a method can destructure your code; so avoid using them
+casually. A well-designed method has well-defined exit points.
+To review: A void method can return in one of two ways—its closing curly brace is
+reached, or a return statement is executed.
+Returning a Value
+Although methods with a return type of void are not rare, most methods will return a value.
+In fact, the ability to return a value is one of the most useful features of a method. You have
+already seen one example of a return value: when we used the sqrt( ) function to obtain a
+square root.
+Return values are used for a variety of purposes in programming. In some cases, such as
+with sqrt( ), the return value contains the outcome of some calculation. In other cases, the
+return value may simply indicate success or failure. In still others, it may contain a status code.
+Whatever the purpose, using method return values is an integral part of Java programming.114 Java: A Beginner’s Guide
+Methods return a value to the calling routine using this form of return:
+return value;
+Here, value is the value returned. This form of return can be used only with methods that have
+a non-void return type. Furthermore, a non-void method must return a value by using this form
+of return.
+You can use a return value to improve the implementation of range( ). Instead of displaying
+the range, a better approach is to have range( ) compute the range and return this value. Among
+the advantages to this approach is that you can use the value for other calculations. The following
+example modifies range( ) to return the range rather than displaying it.
+// Use a return value.
+class Vehicle {
+int passengers; // number of passengers
+int fuelcap; // fuel capacity in gallons
+int mpg; // fuel consumption in miles per gallon
+// Return the range.
+int range() {
+return mpg * fuelcap;
+}
+}
+class RetMeth {
+public static void main(String args[]) {
+Vehicle minivan = new Vehicle();
+Vehicle sportscar = new Vehicle();
+int range1, range2;
+// assign values to fields in minivan
+minivan.passengers = 7;
+minivan.fuelcap = 16;
+minivan.mpg = 21;
+// assign values to fields in sportscar
+sportscar.passengers = 2;
+sportscar.fuelcap = 14;
+sportscar.mpg = 12;
+// get the ranges
+range1 = minivan.range();
+range2 = sportscar.range();
+Return the range for a given vehicle.
+Assign the value
+returned to a variable.Chapter 4: Introducing Classes, Objects, and Methods 115
+System.out.println("Minivan can carry " + minivan.passengers +
+" with range of " + range1 + " Miles");
+System.out.println("Sportscar can carry " + sportscar.passengers +
+" with range of " + range2 + " miles");
+}
+}
+The output is shown here:
+Minivan can carry 7 with range of 336 Miles
+Sportscar can carry 2 with range of 168 miles
+In the program, notice that when range( ) is called, it is put on the right side of an assignment
+statement. On the left is a variable that will receive the value returned by range( ). Thus, after
+range1 = minivan.range();
+executes, the range of the minivan object is stored in range1.
+Notice that range( ) now has a return type of int. This means that it will return an integer
+value to the caller. The return type of a method is important because the type of data returned
+by a method must be compatible with the return type specified by the method. Thus, if you
+want a method to return data of type double, its return type must be type double.
+Although the preceding program is correct, it is not written as efficiently as it could be.
+Specifically, there is no need for the range1 or range2 variables. A call to range( ) can be
+used in the println( ) statement directly, as shown here:
+System.out.println("Minivan can carry " + minivan.passengers +
+" with range of " + minivan.range() + " Miles");
+In this case, when println( ) is executed, minivan.range( ) is called automatically and its value
+will be passed to println( ). Furthermore, you can use a call to range( ) whenever the range of a
+Vehicle object is needed. For example, this statement compares the ranges of two vehicles:
+if(v1.range() > v2.range()) System.out.println("v1 has greater range");
+
+
+
+
+
+
