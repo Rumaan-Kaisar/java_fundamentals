f8ac2c23ecbd256b4b6de071bdd18819e58564db
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-24 15:12:45 +0600

    Update jv_ch02_03_2_methods_parameters.java

--- JAVA_ch_02_class_obj_array/jv_ch02_03_2_methods_parameters.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_03_2_methods_parameters.java
@@ -1,11 +1,27 @@
 
-/* -=-=-=-=-=-=-=-=-=-=-=      methods with parameters      -=-=-=-=-=-=-=-=-=-=-= 
+// 115.9-124.7
 
-2.4 Methods with parameters
-A value passed to a method is called an argument. Inside the method, the variable that receives the argument is called a parameter.  Example: devide(int x, int y)  here x and y are parameters but in  devide(2, 3)  2 and 3 are arguments.
-	A parameter is within the scope of its method, and aside from its special task of receiving an argument, it acts like any other local variable.
-	A method can have more than one parameter which separatef one from the next with a comma. Eg: 	devide(int x, int y)
-	When using multiple parameters, each parameter specifies its own type, which can differ from the others. For example, this is perfectly valid:		int myMeth(int a, double b, float c){  }
+/* -=-=-=-=-=-=-=-=-=-=-=      methods with parameters      -=-=-=-=-=-=-=-=-=-=-= 
+    parameters and arguments:
+        A value passed to a method is called an "argument". 
+        Inside the method, the variable that receives the argument is called a "parameter".  
+        Example: 
+                    devide(int x, int y)
+            
+            here x and y are parameters but in    devide(2, 3)    2 and 3 are arguments.
+
+    Scope of parameters:
+        A parameter is within the scope of its method,
+        aside from its special task of receiving an argument, it acts like any other local variable.
+
+    A method can have more than one parameter       devide(int x, int y)
+
+
+    multiple parameters:
+        When a method has multiple parameters, each parameter specifies its own type, which can differ from the others. 
+        For example, this is perfectly valid:
+        
+                    int myMeth(int a, double b, float c){  }
 
 
 
