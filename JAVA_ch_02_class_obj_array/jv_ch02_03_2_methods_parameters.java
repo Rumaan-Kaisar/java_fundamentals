2df47664e4d3ec5c8e05cc3762ab6afab7c063b3
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-28 14:24:58 +0600

    Update jv_ch02_03_2_methods_parameters.java

--- JAVA_ch_02_class_obj_array/jv_ch02_03_2_methods_parameters.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_03_2_methods_parameters.java
@@ -29,13 +29,18 @@
 */
 
 
-// ----  rev [24-Apr-2025]  ----
+
 
 /* Example 1: Here is a simple example that uses a parameter. Inside the ChkNum class, 
                 the method isEven() returns true if the value that it is passed is even. 
                 It returns false otherwise. 
                 
                 Therefore, isEven() has a return type of "boolean"
+
+
+                isEven() is called three times with different values.
+                    Each time, a value (called an argument) is passed inside the parentheses. 
+                    When isEven() is called, its parameter x takes the value of the argument.
 */
 
 // NOTE: In C/C++, functions can exist outside of classes, while in Java, methods must be inside a class.
@@ -50,68 +55,19 @@ class ChkNum {
 class ParmDemo{
     public static void main(String args[]) {
         ChkNum e = new ChkNum();    // creating a class of ChkNum to use the method isEven() encapsulated in it
-if(e.isEven(10)) System.out.println("10 is even.");
-if(e.isEven(9)) System.out.println("9 is even.");
-if(e.isEven(8)) System.out.println("8 is even.");
-}
-}
 
-
-
-
-
-
-
-// ------------   add below to previous :: jv_ch02_03_1_methods_return.java    ------------
-
-// In Java, to create a method, it must be inside a class. 
-// It's not necessary for a class to always have member variables; it can just contain methods only
-
-// The purpose of the following RetInt class is only to define the IntRand() method. In Java, methods must be inside a class, but in C++, we can define a function directly without a class.
-
-// In Java, to create a method, it must be inside a class. 
-// A class can have one or more methods, like this:
-
-class RetInt {
-    // This method will return a random integer
-    int IntRand() {
-        return (int)(Math.random() * 100); // returns a random integer between 0 and 99
-    }
-}
-
-class MethDemo {
-    public static void main(String args[]) {
-        // Create an object of RetInt to use the IntRand() method
-        RetInt makeint = new RetInt();
-        
-        // Call the IntRand() method and display the returned value
-        System.out.println("A random integer: " + makeint.IntRand());
+        if(e.isEven(10)) System.out.println("10 is even.");
+        if(e.isEven(9)) System.out.println("9 is even.");
+        if(e.isEven(8)) System.out.println("8 is even.");
     }
 }
 
 
 
+// ----  rev [24-Apr-2025]  ----
 
 
 
-
-
-
-
-
-
-
-
-Here is the output produced by the program:
-10 is even.
-8 is even.
-In the program, isEven( ) is called three times, and each time a different value is passed.
-Let’s look at this process closely. First, notice how isEven( ) is called. The argument is specified
-between the parentheses. When isEven( ) is called the first time, it is passed the value 10. Thus,
-when isEven( ) begins executing, the parameter x receives the value 10. In the second call, 9 is
-the argument, and x, then, has the value 9. In the third call, the argument is 8, which is the value
-that x receives. The point is that the value passed as an argument when isEven( ) is called is the
-value received by its parameter, x.
 
 
 /* Example 2: A method can have more than one parameter. Simply declare each parameter, separating
