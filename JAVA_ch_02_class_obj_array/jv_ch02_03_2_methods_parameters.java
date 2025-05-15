9cf2f9231056683228784998dc0f669b4d8698ed
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-05-15 19:26:29 +0600

    Update jv_ch02_03_2_methods_parameters.java

--- JAVA_ch_02_class_obj_array/jv_ch02_03_2_methods_parameters.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_03_2_methods_parameters.java
@@ -223,52 +223,6 @@ class Help3 {
             if(choice == 'q') break;
             System.out.println("\n");
 
-            switch(choice) {
-                case '1':
-                    System.out.println("The if:\n");
-                    System.out.println("if(condition) statement;");
-                    System.out.println("else statement;");
-                    break;
-
-                case '2':
-                    System.out.println("The switch:\n");
-                    System.out.println("switch(expression) {");
-                    System.out.println(" case constant:");
-                    System.out.println(" statement sequence");
-                    System.out.println(" break;");
-                    System.out.println(" // ...");
-                    System.out.println("}");
-                    break;
-
-                case '3':
-                    System.out.println("The for:\n");
-                    System.out.print("for(init; condition; iteration)");
-                    System.out.println(" statement;");
-                    break;
-
-                case '4':
-                    System.out.println("The while:\n");
-                    System.out.println("while(condition) statement;");
-                    break;
-
-                case '5':
-                    System.out.println("The do-while:\n");
-                    System.out.println("do {");
-                    System.out.println(" statement;");
-                    System.out.println("} while (condition);");
-                    break;
-
-                case '6':
-                    System.out.println("The break:\n");
-                    System.out.println("break; or break label;");
-                    break;
-
-                case '7':
-                    System.out.println("The continue:\n");
-                    System.out.println("continue; or continue label;");
-                    break;
-            }
-            System.out.println();
         }
     }
 }
@@ -277,54 +231,59 @@ class Help3 {
 
 
 // The entire listing for HelpClassDemo.java is shown here:
-/*
-Try This 4-1
-Convert the help system from Try This 3-3 into
-a Help class.
-*/
+
+
+// Define help method 
 class Help {
-void helpOn(int what) {
-switch(what) {
-case '1':
-System.out.println("The if:\n");
-System.out.println("if(condition) statement;");
-System.out.println("else statement;");
-break;Chapter 4: Introducing Classes, Objects, and Methods 123
-case '2':
-System.out.println("The switch:\n");
-System.out.println("switch(expression) {");
-System.out.println(" case constant:");
-System.out.println(" statement sequence");
-System.out.println(" break;");
-System.out.println(" // ...");
-System.out.println("}");
-break;
-case '3':
-System.out.println("The for:\n");
-System.out.print("for(init; condition; iteration)");
-System.out.println(" statement;");
-break;
-case '4':
-System.out.println("The while:\n");
-System.out.println("while(condition) statement;");
-break;
-case '5':
-System.out.println("The do-while:\n");
-System.out.println("do {");
-System.out.println(" statement;");
-System.out.println("} while (condition);");
-break;
-case '6':
-System.out.println("The break:\n");
-System.out.println("break; or break label;");
-break;
-case '7':
-System.out.println("The continue:\n");
-System.out.println("continue; or continue label;");
-break;
-}
-System.out.println();
-}
+    void helpOn(int choice) {
+        switch(choice) {
+            case '1':
+                System.out.println("The if:\n");
+                System.out.println("if(condition) statement;");
+                System.out.println("else statement;");
+                break;
+
+            case '2':
+                System.out.println("The switch:\n");
+                System.out.println("switch(expression) {");
+                System.out.println(" case constant:");
+                System.out.println(" statement sequence");
+                System.out.println(" break;");
+                System.out.println(" // ...");
+                System.out.println("}");
+                break;
+
+            case '3':
+                System.out.println("The for:\n");
+                System.out.print("for(init; condition; iteration)");
+                System.out.println(" statement;");
+                break;
+
+            case '4':
+                System.out.println("The while:\n");
+                System.out.println("while(condition) statement;");
+                break;
+
+            case '5':
+                System.out.println("The do-while:\n");
+                System.out.println("do {");
+                System.out.println(" statement;");
+                System.out.println("} while (condition);");
+                break;
+
+            case '6':
+                System.out.println("The break:\n");
+                System.out.println("break; or break label;");
+                break;
+
+            case '7':
+                System.out.println("The continue:\n");
+                System.out.println("continue; or continue label;");
+                break;
+        }
+        System.out.println();
+    }
+
 
 
 void showMenu() {
@@ -338,12 +297,16 @@ System.out.println(" 6. break");
 System.out.println(" 7. continue\n");
 System.out.print("Choose one (q to quit): ");
 }
-(continued)124 Java: A Beginner’s Guide
+
 boolean isValid(int ch) {
 if(ch < '1' | ch > '7' & ch != 'q') return false;
 else return true;
 }
 }
+
+
+
+
 class HelpClassDemo {
 public static void main(String args[])
 throws java.io.IOException {
