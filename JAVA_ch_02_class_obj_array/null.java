7c1bf4d5b698352e0e90e8c56ea2a79facc9ca6d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-08-18 14:00:30 +0600

    Update null.java

--- JAVA_ch_02_class_obj_array/null.java
+++ JAVA_ch_02_class_obj_array/null.java
@@ -8,11 +8,8 @@
 
 /*  
 
-A method that returns a value must return via the return statement, passing .
 
 
-7. What name does a constructor have?
-A constructor has the same name as its class.
 
 
 9. What is garbage collection and how does it work? What is finalize( )?
@@ -20,8 +17,6 @@ Garbage collection is the mechanism that recycles unused objects so that their m
 An object’s finalize( ) method is called just prior to an object being recycled.
 
 
-What is "this"?
-The this keyword is a reference to the object on which a method is invoked. 
 
 
 
@@ -29,45 +24,22 @@ The this keyword is a reference to the object on which a method is invoked.
 
 
 
+*/
 
 
+// Constructors
 
 
-11. Can a constructor have one or more parameters?
-Yes.
+// Memory Management
 
+/*  Example 2:  What is garbage collection?
+                What is finalize()?
 
-12. If a method returns no value, what must its return type be?
-void
-
-
-
-
-Here's a clear, grouped and pointwise version of your Q&A, 
-with simple wording so it’s easy to follow:
-
-
-
-3️⃣ Constructors
-Q: What name does a constructor have?
-A:
-
-Same name as its class.
-
-Q: Can a constructor have one or more parameters?
-A:
-
-Yes, it can have any number of parameters.
-
-
-
-4️⃣ Memory Management
-Q: What is garbage collection?
 A:
 
 Process that automatically removes unused objects so their memory can be reused.
 
-Q: What is finalize()?
+Q: 
 A:
 
 A method called just before an object is garbage collected.
