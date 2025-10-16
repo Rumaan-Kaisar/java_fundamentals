867f9547e7335a57e334cb2ffe610bfb505d9b5a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-10-16 15:33:16 +0600

    Update jv_ch02_07_2_MultiD_irregular.java

--- JAVA_ch_02_class_obj_array/jv_ch02_07_2_MultiD_irregular.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_07_2_MultiD_irregular.java
@@ -288,3 +288,53 @@ class TwoDArrayExample {
     }
 }
 
+
+
+
+/* Example 2: The following program stores the number of passengers on an airport shuttle. 
+                The shuttle runs 10 times a day on weekdays and 2 times a day on weekends. 
+
+                So, in the riders array, 
+                    the first five rows have 10 elements each, and 
+                    the last two rows have 2 elements each. 
+*/
+
+class Ragged {
+    public static void main(String args[]) {
+        int riders[][] = new int[7][];
+
+        // ----  Manually allocate differing size second dimensions.  ----
+        
+        riders[0] = new int[10];    // the second dimensions are size of 10
+        riders[1] = new int[10];
+        riders[2] = new int[10];
+        riders[3] = new int[10];
+        riders[4] = new int[10];
+        riders[5] = new int[2];     // but these the second dimensions are size of 2 elements
+        riders[6] = new int[2];
+
+        int i, j;
+        // fabricate some fake data
+        for(i=0; i<5; i++) for(j=0; j<10; j++) riders[i][j] = i + j + 10;
+        for(i=5; i<7; i++) for(j=0; j<2; j++) riders[i][j] = i + j + 10;
+
+        System.out.println("Riders per trip during the week:");
+
+        for(i=0; i<5; i++) {
+            for(j=0; j<10; j++) System.out.print(riders[i][j] + " ");
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Riders per trip on the weekend:");
+
+        for(i=5; i<7; i++) { 
+            for(j=0; j<2; j++) System.out.print(riders[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// ----  rev[16-Oct-2025]  BOK:144  ----
+
