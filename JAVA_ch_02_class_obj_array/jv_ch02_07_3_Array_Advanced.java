c9283ce47bb771e80d286b3f8fa70e00900045fd
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-10-27 07:58:13 +0600

    Update jv_ch02_07_3_Array_Advanced.java

--- JAVA_ch_02_class_obj_array/jv_ch02_07_3_Array_Advanced.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_07_3_Array_Advanced.java
@@ -39,7 +39,52 @@
 // rev[23-Oct-2025] rev book
 
 
-/* Example 1: */
+/* Example 1: 
+
+As with other objects, when you assign one array reference variable to another, you are simply
+changing what object that variable refers to. You are not causing a copy of the array to be
+made, nor are you causing the contents of one array to be copied to the other. For example,
+consider this program:
+// Assigning array reference variables.
+class AssignARef {
+public static void main(String args[]) {
+int i;
+int nums1[] = new int[10];
+int nums2[] = new int[10];
+for(i=0; i < 10; i++)
+nums1[i] = i;
+for(i=0; i < 10; i++)
+nums2[i] = -i;Chapter 5: More Data Types and Operators 147
+System.out.print("Here is nums1: ");
+for(i=0; i < 10; i++)
+System.out.print(nums1[i] + " ");
+System.out.println();
+System.out.print("Here is nums2: ");
+for(i=0; i < 10; i++)
+System.out.print(nums2[i] + " ");
+System.out.println();
+nums2 = nums1; // now nums2 refers to nums1
+System.out.print("Here is nums2 after assignment: ");
+for(i=0; i < 10; i++)
+System.out.print(nums2[i] + " ");
+System.out.println();
+// now operate on nums1 array through nums2
+nums2[3] = 99;
+System.out.print("Here is nums1 after change through nums2: ");
+for(i=0; i < 10; i++)
+System.out.print(nums1[i] + " ");
+System.out.println();
+}
+}
+The output from the program is shown here:
+Here is nums1: 0 1 2 3 4 5 6 7 8 9
+Here is nums2: 0 -1 -2 -3 -4 -5 -6 -7 -8 -9
+Here is nums2 after assignment: 0 1 2 3 4 5 6 7 8 9
+Here is nums1 after change through nums2: 0 1 2 99 4 5 6 7 8 9
+As the output shows, after the assignment of nums1 to nums2, both array reference variables
+refer to the same object.
+
+*/
 
 
 /*  
