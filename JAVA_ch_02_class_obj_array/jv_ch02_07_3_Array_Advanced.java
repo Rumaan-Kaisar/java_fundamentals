e5b6db2dfa4aab7bb85e2c4064fede84146269b5
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-11-06 08:09:18 +0600

    Update jv_ch02_07_3_Array_Advanced.java

--- JAVA_ch_02_class_obj_array/jv_ch02_07_3_Array_Advanced.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_07_3_Array_Advanced.java
@@ -64,8 +64,8 @@ class AssignARef {
         int nums1[] = new int[10];
         int nums2[] = new int[10];
 
-        for(i=0; i < 10; i++) nums1[i] = i;
-        for(i=0; i < 10; i++) nums2[i] = -i;
+        for(i=0; i < 10; i++) nums1[i] = i;     // putting values to nums1
+        for(i=0; i < 10; i++) nums2[i] = -i;    // putting values to nums2
 
         System.out.print("Here is nums1: ");
         for(i=0; i < 10; i++) System.out.print(nums1[i] + " ");
@@ -96,29 +96,12 @@ class AssignARef {
 
 /*  
 
-                int nums1[] = new int[10];
-                int nums2[] = new int[10];
-
-                for(i=0; i < 10; i++) nums1[i] = i;      // putting values to nums1 
-                for(i=0; i < 10; i++) nums2[i] = -i;     // putting values to nums2
-
-    
-    Output :
-Here is nums1: 0 1 2 3 4 5 6 7 8 9
-Here is nums2: 0 -1 -2 -3 -4 -5 -6 -7 -8 -9
-Here is nums2 after assignment: 0 1 2 3 4 5 6 7 8 9
-nums1 after change through nums2: 0 1 2 99 4 5 6 7 8 9
 
 
-           System.out.print("Here is nums1: "); for(i=0; i < 10; i++) System.out.print(nums1[i] + " "); System.out.println();
-           System.out.print("Here is nums2: "); for(i=0; i < 10; i++) System.out.print(nums2[i] + " "); System.out.println();
 
-           nums2 = nums1; 			// now nums2 refers to nums1
-           System.out.print("Here is nums2 after assignment: "); for(i=0; i < 10; i++) System.out.print(nums2[i] + " "); System.out.println();
 
-           nums2[3] = 99; 			// operating on nums1 array through nums2
-           System.out.print("nums1 after change through nums2: "); for(i=0; i < 10; i++) System.out.print(nums1[i] + " "); System.out.println();
-	As the output shows, after the assignment of nums1 to nums2, both array reference variables refer to the same object.
+
+
 	Using the length Member (instance member of an array object): Because arrays are implemented as objects, each array has associated with it a length instance variable that contains the number of elements that the array can hold. (In other words, length contains the size of the array.) 
 
 
