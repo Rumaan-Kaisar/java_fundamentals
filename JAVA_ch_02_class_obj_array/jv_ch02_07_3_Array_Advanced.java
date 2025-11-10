796cc7caa97408cc244c953c3932540df036c4c7
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-11-10 07:54:19 +0600

    Update jv_ch02_07_3_Array_Advanced.java

--- JAVA_ch_02_class_obj_array/jv_ch02_07_3_Array_Advanced.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_07_3_Array_Advanced.java
@@ -106,6 +106,42 @@ class AssignARef {
 
 
 
+// Use the length array member.
+class LengthDemo {
+public static void main(String args[]) {
+int list[] = new int[10];
+int nums[] = { 1, 2, 3 };
+int table[][] = { // a variable-length table
+{1, 2, 3},
+Assign an array reference.148 Java: A Beginner’s Guide
+{4, 5},
+{6, 7, 8, 9}
+};
+System.out.println("length of list is " + list.length);
+System.out.println("length of nums is " + nums.length);
+System.out.println("length of table is " + table.length);
+System.out.println("length of table[0] is " + table[0].length);
+System.out.println("length of table[1] is " + table[1].length);
+System.out.println("length of table[2] is " + table[2].length);
+System.out.println();
+// use length to initialize list
+for(int i=0; i < list.length; i++)
+list[i] = i * i;
+System.out.print("Here is list: ");
+// now use length to display list
+for(int i=0; i < list.length; i++)
+System.out.print(list[i] + " ");
+System.out.println();
+}
+}
+This program displays the following output:
+length of list is 10
+length of nums is 3
+length of table is 3
+length of table[0] is 3
+length of table[1] is 2
+length of table[2] is 4
+Here is list: 0 1 4 9 16 25 36 49 64 81
 
 
 
@@ -119,7 +155,7 @@ Following program demonstrates this property: */
 
 
 
-// ----  rev[03-Nov-2025] rev book  ----
+// ----  rev[06-Nov-2025] rev book  ----
 
 /*  
 
@@ -153,7 +189,7 @@ System.out.println();
 	      Here is list: 0 1 4 9 16 25 36 49 64 81
 
 
-          
+
 	Pay special attention to the way length is used with the two-dimensional array table. Since a two-dimensional array is an array of arrays. 
 	Thus, when " table.length " is used, it obtains the number of arrays stored in table, which is 3 in this case. 
 	To obtain the length of any individual array in table, you will use an expression such as this, " table[0].length " which, in this case, obtains the length of the first array.
