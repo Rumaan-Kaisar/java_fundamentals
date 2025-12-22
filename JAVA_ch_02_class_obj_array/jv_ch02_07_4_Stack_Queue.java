8fe8b57718080ba788734cd08e9ac896713d8ff9
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-12-22 13:05:25 +0600

    Update jv_ch02_07_4_Stack_Queue.java

--- JAVA_ch_02_class_obj_array/jv_ch02_07_4_Stack_Queue.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_07_4_Stack_Queue.java
@@ -120,26 +120,26 @@ class Queue{
 // Demonstrate the Queue class.
 class QDemo { 
     public static void main(String args[]) {
+
         Queue bigQ = new Queue(100);
         Queue smallQ = new Queue(4);
         char ch;
         int i;
 
+        System.out.println("Using bigQ to store the alphabet.");
+        for(i=0; i < 26; i++) bigQ.put((char)('A' + i));    // put numbers 
 
+        System.out.print("Contents of bigQ: ");         // retrieve and display 
+        for(i=0; i < 26; i++) { 
+            ch = bigQ.get();
+            if(ch != (char)0) System.out.print(ch); 
+        }
+        System.out.println("\n");
 
-
-System.out.println("Using bigQ to store the alphabet.");
-for(i=0; i < 26; i++) bigQ.put((char) ('A' + i));         // put numbers 
-
-System.out.print("Contents of bigQ: ");       // retrieve and display 
-for(i=0; i < 26; i++) { ch = bigQ.get();
-     if(ch != (char) 0) System.out.print(ch); }
-System.out.println("\n");
-
-System.out.println("Using smallQ to generate errors.");
-for(i=0; i < 5; i++) { 
-           System.out.print("Attempt to store " + (char) ('Z' - i));
-           smallQ.put((char) ('Z' - i));
+        System.out.println("Using smallQ to generate errors.");
+        for(i=0; i < 5; i++) { 
+           System.out.print("Attempt to store " + (char)('Z' - i));
+           smallQ.put((char)('Z' - i));
            System.out.println();   }
 System.out.println();
 
