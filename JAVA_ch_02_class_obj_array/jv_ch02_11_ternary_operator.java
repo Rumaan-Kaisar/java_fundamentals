c8e00b3a327c06470f0d95b504dc9fceec8e70ed
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-03-10 18:43:02 +0600

    Update jv_ch02_11_ternary_operator.java

--- JAVA_ch_02_class_obj_array/jv_ch02_11_ternary_operator.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_11_ternary_operator.java
@@ -1,7 +1,7 @@
 
-/* -=-=-=-=-=-=-=-=-=-=-=      chapter name      -=-=-=-=-=-=-=-=-=-=-= */
-
 // jv_ch02_11_ternary_operator         2.18        176.8-179
+
+/* -=-=-=-=-=-=-=-=-=-=-=      The '?' ternary Operator      -=-=-=-=-=-=-=-=-=-=-=
 
 2.18 The ? ternary Operator
 The ? is called a ternary operator because it requires three operands. General form: 		Exp1 ? Exp2 : Exp3;
@@ -9,3 +9,5 @@ The ? is called a ternary operator because it requires three operands. General f
 	The value of a ? expression is determined like this: Exp1 is evaluated. If it is true, then Exp2 is evaluated and becomes the value of the entire ? expression. If Exp1 is false, then Exp3 is evaluated and its value becomes the value of the expression. 
 	Example, Prevent a division by zero using the ?, 	if(i != 0 ? true : false) System.out.println("100 / " + i + " is " + 100 / i);
 	If i=0, then the outcome of the if is false, the division by zero is prevented, and no result is displayed. Otherwise, the division performed.
+
+*/
