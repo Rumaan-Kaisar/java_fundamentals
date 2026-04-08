dd819fcf56ea44e81dfa0039294eab7e98702e0e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-04-09 04:43:52 +0600

    Update jv_ch02_11_ternary_operator.java

--- JAVA_ch_02_class_obj_array/jv_ch02_11_ternary_operator.java
+++ JAVA_ch_02_class_obj_array/jv_ch02_11_ternary_operator.java
@@ -4,9 +4,17 @@
 /* -=-=-=-=-=-=-=-=-=-=-=      The '?' ternary Operator      -=-=-=-=-=-=-=-=-=-=-=
 
     The "?" is called a TERNARY operator because it requires "three operands".
-    General form:       Exp1 ? Exp2 : Exp3;
+    It is similar to C/C++'s "?" operator.
+    
+    General form:
+                    Exp1 ? Exp2 : Exp3;
 
-   where Exp1 is a boolean expression, and Exp2 and Exp3 are expressions of any type other than void. The type of Exp2 and Exp3 must be the same (or compatible), though. Notice the use and placement of the colon. It is similar to C/C++'s "?" operator.
+        where "Exp1" is a boolean expression, and 
+        Exp2 and Exp3 are expressions of any type "other than void".
+        The "type" of Exp2 and Exp3 must be the same (or compatible).
+        Notice the use and placement of the colon ":"
+
+
    The value of a ? expression is determined like this: Exp1 is evaluated. If it is true, then Exp2 is evaluated and becomes the value of the entire ? expression. If Exp1 is false, then Exp3 is evaluated and its value becomes the value of the expression. 
    Example, Prevent a division by zero using the ?,    if(i != 0 ? true : false) System.out.println("100 / " + i + " is " + 100 / i);
    If i=0, then the outcome of the if is false, the division by zero is prevented, and no result is displayed. Otherwise, the division performed.
